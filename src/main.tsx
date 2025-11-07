4c4fd08ce2b09d13091d1acd85bff66e415c1a20
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2025-11-07 15:30:21 +0100

    making it a pwa

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,12 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import { Portraid } from "./Portraid.tsx";
 
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/service-worker.js")
+    .catch((err) => console.error("SW registration failed:", err));
+}
+
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <Portraid />
