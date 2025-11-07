caa862ff90d84907566166024095b32998b9fe39
Bjørn Jarle Kvande <bjorn.jarle.kvande@gmail.com>
2025-11-07 10:47:49 +0100

    initial working commit with vite, react, typescript, tailwind

---
+++ src/Portraid.tsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <div className="text-red-500 text-4xl flex justify-center h-full items-center">
+        Hello World
+      </div>
+    </>
+  );
+}
+
+export default App;
